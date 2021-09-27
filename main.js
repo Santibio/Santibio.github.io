48c18ac4c9b19675f089e52e84563602bf63ac61
Santibio <santiagobioletto@gmail.com>
2021-09-27 08:54:42 -0300

    Se paso todo a para la pagina

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
 const darkModeSwitch = document.getElementById("moon");
 const body = document.body;
 const navbar = document.querySelector(".navbar");
-const contactMe = document.querySelector(".contact-me");
+const welcomeTitle = document.getElementById("about")
+const welcomeText = document.querySelector(".welcome-text");
+const projectTitle = document.querySelector(".projects-title")
+const contactMe = document.getElementById("contact");
+const contactMeTitle = document.querySelector(".contact-me");
 
-let darkSwitch = false
+const darkMode = () => {
+  navbar.classList.replace("navbar-light", "navbar-dark");
+  navbar.classList.add("bg-dark");
+  body.style.backgroundColor = "#212529";
+  welcomeTitle.style.color = "#fff";
+  welcomeText.style.color = "#fff";
+  projectTitle.style.color = "#fff";
+  contactMe.style.backgroundColor = "#fff";
+  darkModeSwitch.classList.replace("far", "fas");
+  localStorage.setItem("dark-mode", "true");
+};
+const lightMode = () => {
+  navbar.classList.replace("navbar-dark", "navbar-light");
+  navbar.classList.remove("bg-dark");
+  body.style.backgroundColor = "";
+  welcomeTitle.style.color = "#000";
+  welcomeText.style.color = "#000";
+  contactMeTitle.style.color = "#000";
+  projectTitle.style.color = "#000";
+  darkModeSwitch.classList.replace("fas", "far");
+  localStorage.setItem("dark-mode", "true");
+  localStorage.setItem("dark-mode", "false");
+};
 
 darkModeSwitch.addEventListener("click", () => {
   if (navbar.classList.contains("navbar-light")) {
-    navbar.classList.replace("navbar-light", "navbar-dark");
-    navbar.classList.add("bg-dark");
-    body.style.backgroundColor = "#212529";
-    body.style.color = "#fff";
-    contactMe.style.color = "#fff";
-    darkSwitch = true
-    darkModeSwitch.classList.replace("far", "fas")
-    console.log(darkSwitch)
+    darkMode();
   } else {
-    navbar.classList.replace("navbar-dark", "navbar-light");
-    navbar.classList.remove("bg-dark");
-    body.style.backgroundColor = "";
-    body.style.color = "#000";
-    contactMe.style.color = "#000";
-    darkSwitch = false
-    darkModeSwitch.classList.replace("fas", "far")
-    console.log(darkSwitch)
+    lightMode();
   }
 });
 
+if (localStorage.getItem("dark-mode") === "true") {
+  darkMode();
+} else {
+  lightMode();
+}
+
 window.addEventListener("scroll", () => {
   navbar.classList.toggle("sticky", window.scrollY > 0);
-  console.log(darkSwitch)
-  if (navbar.classList.contains("sticky")) {
-    navbar.classList.replace("navbar-light", "navbar-dark");
-  }  
-  else if( navbar.classList.contains("navbar-light") && darkSwitch == true){
-    navbar.classList.replace("navbar-light", "navbar-dark")
-  }
-  else if(darkSwitch == false){
-    navbar.classList.replace("navbar-dark", "navbar-light");
-  }
+  navbar.classList.toggle("bg-light", window.scrollY > 0);
 });
